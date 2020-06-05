7e816025dff0e1d527bffae21e2ba4b93ce33d9f
elijahdaniel <e.paminsan@gmail.com>
2020-06-05 14:19:37 -0700

    bubble and notes from lecture

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,5 @@
+# selection sort - loop in a loop. O(n^2) - array at every step
+
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
@@ -7,7 +9,7 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-        
+
         # range: start at current + 1, stop at length of arr
         for j in range(cur_index + 1, len(arr)):
             # if smallest > loop cycle j position in array
@@ -15,9 +17,9 @@ def selection_sort(arr):
                 # if true, set smallest to j
                 smallest_index = j
 
-
         # TO-DO: swap
         # Your code here
+        # unpacking and assigning to a tuple
         arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
@@ -26,11 +28,27 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    made_a_swap = True
 
+    # while there is a swap
+    while made_a_swap:
+        # set made_a_swap to false
+        made_a_swap = False
+
+        # start at 0 to element before end of length of array
+        for i in range(0, len(arr) - 1):
+            # counter
+            j = i + 1
+
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+                # swap was made. set to true
+                made_a_swap = True
 
     return arr
 
-'''
+
+"""
 STRETCH: implement the Count Sort function below
 
 Counting sort is a sorting algorithm that works on a set of data where
@@ -46,9 +64,8 @@ showed up, we can construct a sorted set of the input data from the
 buckets. 
 
 What is the time and space complexity of the counting sort algorithm?
-'''
-def counting_sort(arr, maximum=None):
-    # Your code here
+"""
 
 
+def count_sort(arr, maximum=0):
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -4,28 +4,29 @@ def linear_search(arr, target):
         if arr[i] == target:
             return i
 
-    return -1   # not found
+    return -1  # not found
 
 
 # Write an iterative implementation of Binary Search
+# binary - half
 def binary_search(arr, target):
 
     # Your code here
     left = 0
     mid = 0
     right = len(arr) - 1
-  
-    while left <= right: 
+
+    while left <= right:
         # set mid to sum of h & l with floor division
         mid = (right + left) // 2
-        # check if target is at mid 
-        if arr[mid] < target: 
+        # check if target is at mid
+        if arr[mid] < target:
             left = mid + 1
-        # if target is greater, ignore left half 
-        elif arr[mid] > target: 
+        # if target is greater, ignore left half
+        elif arr[mid] > target:
             right = mid - 1
-        # if target is smaller, ignore right half 
-        else: 
-            return mid 
+        # if target is smaller, ignore right half
+        else:
+            return mid
 
     return -1  # not found
